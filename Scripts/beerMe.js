e2f1598933ac9d895069fc7c5286eaa7091eeddb
Cody Mumford <cody.mumford9106@hotmail.com>
2019-07-10 18:20:51 -0500

    dropdownList.addEventListener to display a list of breweries in a state

--- Scripts/beerMe.js
+++ Scripts/beerMe.js
@@ -39,23 +39,68 @@ button.addEventListener('click', async function (e) {
     cardBody.appendChild(p);
 
 });
+
 const stateMenu = document.getElementById('StateMenu');
 stateMenu.addEventListener('load', addStates(states));
 
 function addStates(states) {
     states.forEach(state => {
-        console.dir(state);
         const stateButton = document.createElement('button');
         stateButton.setAttribute('class', 'dropdown-item');
         stateButton.setAttribute('type', 'button');
+        stateButton.setAttribute('id', 'state');
         stateButton.setAttribute('value', state);
         stateButton.innerHTML = state.toUpperCase();
         stateMenu.appendChild(stateButton);
     });
 };
 
+
+let dropdownList = document.getElementById('StateMenu');
+dropdownList.addEventListener('click', async function (e) {
+    let state = document.querySelector('#state').innerHTML;
+
+    let url = 'https://api.openbrewerydb.org/breweries' + filterByState(state);
+    console.dir(url);
+
+    const response = await fetch(url);
+    console.dir(response);
+    const brewery = await response.json();
+    console.dir(brewery);
+    for (var i = 0; i < brewery.length; i++) {
+        const card = document.createElement('div');
+        card.setAttribute('class', 'card');
+
+        const cardBody = document.createElement('div');
+        cardBody.setAttribute('class', 'card-body');
+
+        const h5 = document.createElement('h5');
+        h5.setAttribute('class', 'card-title');
+        h5.setAttribute('id', 'breweryName');
+        h5.innerHTML = 'Brewery Name: ' + brewery[i].name;
+        const location = brewery[i].city + ', ' + brewery[i].state;
+
+        const p = document.createElement('p');
+        p.setAttribute('class', 'card-text');
+        p.setAttribute('id', 'brewery');
+        p.innerHTML = 'Phone: ' + brewery[i].phone + '<br>' + 'City, State: ' + location;
+
+        const breweryList = document.getElementById('breweryList');
+        //randomBeer.removeChild(randomBeer.childNodes[0]);
+        breweryList.appendChild(card);
+        card.appendChild(cardBody);
+        cardBody.appendChild(h5);
+        cardBody.appendChild(p);
+    }
+
+});
+
+
+
+
+
 function filterByState(input) {
-    return '?by_name=' + input;
+    return '?by_state=' + input;
 };
 
 
