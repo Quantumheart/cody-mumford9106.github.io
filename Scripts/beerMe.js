0d0c963f60911608cbdd5cdcdd0da91725d745d9
Cody Mumford <cody.mumford9106@hotmail.com>
2019-07-10 19:55:50 -0500

    dropdownList fix

--- Scripts/beerMe.js
+++ Scripts/beerMe.js
@@ -48,7 +48,7 @@ function addStates(states) {
         const stateButton = document.createElement('button');
         stateButton.setAttribute('class', 'dropdown-item');
         stateButton.setAttribute('type', 'button');
-        stateButton.setAttribute('id', 'state');
+        stateButton.setAttribute('id', state);
         stateButton.setAttribute('value', state);
         stateButton.innerHTML = state.toUpperCase();
         stateMenu.appendChild(stateButton);
@@ -58,15 +58,17 @@ function addStates(states) {
 
 let dropdownList = document.getElementById('StateMenu');
 dropdownList.addEventListener('click', async function (e) {
-    let state = document.querySelector('#state').innerHTML;
+    
+    let state = e.target.id;
 
     let url = 'https://api.openbrewerydb.org/breweries' + filterByState(state);
     console.dir(url);
 
     const response = await fetch(url);
-    console.dir(response);
     const brewery = await response.json();
     console.dir(brewery);
+    let breweryList = document.getElementById('breweryList');
+    breweryList.innerHTML = "Breweries in: " + state;
     for (var i = 0; i < brewery.length; i++) {
         const card = document.createElement('div');
         card.setAttribute('class', 'card');
@@ -85,7 +87,7 @@ dropdownList.addEventListener('click', async function (e) {
         p.setAttribute('id', 'brewery');
         p.innerHTML = 'Phone: ' + brewery[i].phone + '<br>' + 'City, State: ' + location;
 
-        const breweryList = document.getElementById('breweryList');
+
         //randomBeer.removeChild(randomBeer.childNodes[0]);
         breweryList.appendChild(card);
         card.appendChild(cardBody);
@@ -94,10 +96,6 @@ dropdownList.addEventListener('click', async function (e) {
     }
 
 });
-
-
-
-
 
 function filterByState(input) {
     return '?by_state=' + input;
