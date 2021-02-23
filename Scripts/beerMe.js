b49e96d65acca95f180562c215bedd97455d4c8d
Cody <Mumford>
2021-02-23 16:51:09 -0600

    update spelling

--- Scripts/beerMe.js
+++ Scripts/beerMe.js
@@ -2,7 +2,7 @@
 
 const states = ['alabama', 'alaska', 'arizona', 'arkansas',
     'california', 'colorado', 'connecticut', 'delaware', 'florida', 'georgia', 'hawaii',
-    'idaho', 'illinois', 'indiana', 'iowa', 'indiana', 'kansas', 'kentucky', 'louisana',
+    'idaho', 'illinois', 'indiana', 'iowa', 'indiana', 'kansas', 'kentucky', 'louisiana',
     'maine', 'maryland', 'massachusetts', 'michigan', 'minnesota', 'mississippi',
     'missouri', 'montana', 'nebraska', 'nevada', 'new_hampshire', 'new_jersey', 'new_mexico',
     'new_york', 'north_carolina', 'north_dakota', 'ohio', 'oklahoma', 'oregon', 'pennsylvania',
